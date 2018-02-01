a03357a68399349dbaa16e9cf5564b31f05ccdf9
Jon Ablondi <jonnyablondi@hotmail.com>
2018-02-01 12:08:07 +1100

    linter updates on formatting

--- React-api/models/profile.js
+++ React-api/models/profile.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
-const db = mongoose.connection;
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
+const db = mongoose.connection
 
-db.on('open', () => {console.log('Successful connection to MongoDB')})
+db.on('open', () => { console.log('Successful connection to MongoDB') })
 
-mongoose.connect('mongodb://localhost/profiles',{useMongoClient: true});
+mongoose.connect('mongodb://localhost/profiles', {useMongoClient: true})
 
 const profileSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   age: Number
-});
+})
 
-const Profile = mongoose.model('Profile', profileSchema);
+const Profile = mongoose.model('Profile', profileSchema)
 
-module.exports = Profile;
+module.exports = Profile
 
 // const Profiles = [
 //   {
